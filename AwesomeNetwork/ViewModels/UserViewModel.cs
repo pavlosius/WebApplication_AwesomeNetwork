9c72ff9d0e1ae35603bd0be28fb30711933d0100
pavlosius <pavlosius@mail.ru>
2024-04-04 19:00:48 +0300

    Задание 35.4.1 Добавляет UserViewModel

---
+++ AwesomeNetwork/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AwesomeNetwork.Models.Users;
+
+namespace AwesomeNetwork.ViewModels
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+
+        public UserViewModel(User user)
+        {
+            User = user;
+        }
+
+    }
+}
