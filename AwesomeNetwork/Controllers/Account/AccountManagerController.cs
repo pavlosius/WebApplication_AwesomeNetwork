e6cdcde59bb66031a36c2763e1dabfa46e812543
pavlosius <pavlosius@mail.ru>
2024-04-06 18:42:40 +0300

    Задание 35.4.12 Добавляет метод удаления пользователей DeleteFriend в контроллер AccountManagerController

--- AwesomeNetwork/Controllers/Account/AccountManagerController.cs
+++ AwesomeNetwork/Controllers/Account/AccountManagerController.cs
@@ -223,5 +223,23 @@ namespace AwesomeNetwork.Controllers.Account
             return RedirectToAction("MyPage", "AccountManager");
 
         }
+
+        [Route("DeleteFriend")]
+        [HttpPost]
+        public async Task<IActionResult> DeleteFriend(string id)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            repository.DeleteFriend(result, friend);
+
+            return RedirectToAction("MyPage", "AccountManager");
+
+        }
     }
 }
