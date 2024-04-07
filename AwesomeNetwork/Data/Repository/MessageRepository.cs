c3c4a430279b933ec2fa6278746b38c92905cfca
pavlosius <pavlosius@mail.ru>
2024-04-07 15:51:46 +0300

    Задание 35.5.2 Дополняет класс Меssage. Добавляет метод GegMessages в репозиторий сообщений MessageRepository.

--- AwesomeNetwork/Data/Repository/MessageRepository.cs
+++ AwesomeNetwork/Data/Repository/MessageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AwesomeNetwork.Models.Users;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,21 @@ namespace AwesomeNetwork.Data.Repository
         : base(db)
         {
 
+        }
+
+        public List<Message> GetMessages(User sender, User recipient)
+        {
+            Set.Include(x => x.Recipient);
+            Set.Include(x => x.Sender);
+
+            var from = Set.AsEnumerable().Where(x => x.SenderId == sender.Id && x.RecipientId == recipient.Id).ToList();
+            var to = Set.AsEnumerable().Where(x => x.SenderId == recipient.Id && x.RecipientId == sender.Id).ToList();
+
+            var itog = new List<Message>();
+            itog.AddRange(from);
+            itog.AddRange(to);
+            itog.OrderBy(x => x.Id);
+            return itog;
         }
     }
 }
--- AwesomeNetwork/Models/Users/Message.cs
+++ AwesomeNetwork/Models/Users/Message.cs
@@ -10,8 +10,10 @@ namespace AwesomeNetwork.Models.Users
         public int Id { get; set; }
         public string Text { get; set; }
 
+        public string SenderId { get; set; }
         public User Sender { get; set; }
 
+        public string RecipientId { get; set; }
         public User Recipient { get; set; }
     }
 }
